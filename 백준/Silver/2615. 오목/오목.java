7afe824b3e755cc4d29cd73e9831f8bdd5dcb74c
박시은 <charm516@naver.com>
2025-11-17 21:04:35 +0900

    [Silver I] Title: 오목, Time: 120 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] arr = new int[19][19];
-    // →, ↓, ↘, ↗
     static int[] dx = {0, 1, 1, -1};
     static int[] dy = {1, 0, 1, 1};
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // input
+        arr = new int[19][19];
+
         for (int i = 0; i < 19; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 19; j++) {
@@ -16,13 +19,15 @@ public class Main {
             }
         }
 
-        for (int j = 0; j < 19; j++) {
-            for (int i = 0; i < 19; i++) {
-                if (arr[i][j] == 0) continue;
-                if (solve(i, j)) {
-                    System.out.println(arr[i][j]);
-                    System.out.println((i + 1) + " " + (j + 1));
-                    return;
+        // solve + print
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
+                if (arr[i][j] != 0) {
+                    if (solve(i, j)) {
+                        System.out.println(arr[i][j] == 1 ? 1 : 2);
+                        System.out.println((i + 1) + " " + (j + 1));
+                        return;
+                    }
                 }
             }
         }
@@ -31,34 +36,28 @@ public class Main {
 
     static boolean solve(int x, int y) {
         int color = arr[x][y];
-
         for (int d = 0; d < 4; d++) {
-            int px = x - dx[d];
-            int py = y - dy[d];
-            if (isValid(px, py) && arr[px][py] == color) continue;
+            int cnt = 1;
+            // 이전 칸 체크
+            int bx = x - dx[d];
+            int by = y - dy[d];
+            if (isValid(bx, by) && arr[bx][by] == color) continue;
 
-            int count = 1;
+            // 다음 칸 체크
             int nx = x + dx[d];
             int ny = y + dy[d];
-
             while (isValid(nx, ny) && arr[nx][ny] == color) {
-                count++;
+                cnt++;
                 nx += dx[d];
                 ny += dy[d];
             }
-
-            if (count == 5) {
-                int ex = nx;
-                int ey = ny;
-                if (!isValid(ex, ey) || arr[ex][ey] != color) {
-                    return true;
-                }
-            }
+            if (cnt == 5) return true;
         }
         return false;
     }
 
     static boolean isValid(int x, int y) {
-        return 0 <= x && x < 19 && 0 <= y && y < 19;
+        if (x < 0 || x >= 19 || y < 0 || y >= 19) return false;
+        return true;
     }
 }
